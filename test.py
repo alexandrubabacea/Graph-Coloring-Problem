a0827968d4db9e1d7ffc2ef8966855f1d6637a72
Babacea Alexandru <alexandru.babacea@stud.acs.upb.ro>
2025-01-12 16:12:17 +0200

    Add backtracking test

--- test.py
+++ test.py
@@ -48,7 +48,7 @@ def good_order_crown_graph():
     graph = read_graph_from_file("good_order_crown_graph.in")
     
     print(f"Best order case of crown graph with {len(graph)} nodes:")
-    display_graph(graph)
+    # display_graph(graph)
     
     G = nx.Graph()
     for node, neighbors in enumerate(graph):
@@ -90,7 +90,7 @@ def bad_order_crown_graph():
     graph = read_graph_from_file("bad_order_crown_graph.in")
     
     print(f"Worst order case of crown graph with {len(graph)} nodes:")
-    display_graph(graph)
+    # display_graph(graph)
     
     G = nx.Graph()
     for node, neighbors in enumerate(graph):
@@ -214,11 +214,53 @@ def bad_order_chordal_graph():
         
     print()
     
+############################# backtracking graph #############################
+def backtracking_graph():
+    graph = read_graph_from_file("backtracking_graph.in")
+    
+    G = nx.Graph()
+    for node, neighbors in enumerate(graph):
+        for neighbor in neighbors:
+            G.add_edge(node, neighbor)
+
+    print(f"Backtracking graph with {len(graph)} nodes:")
+    display_graph(graph)
+    
+    start_time = time.time()
+    coloring_backtracking = backtr.backtr_optimal(graph)
+    end_time = time.time()
+    plot_graph(G, coloring_backtracking, "./images/graph_backtracking.png")
+    colors_nr = len(set(coloring_backtracking)) 
+    print(f"Backtracking used {colors_nr} colors.")
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time:.6f} seconds")
+    
+    start_time = time.time()
+    coloring_greedy = greedy.greedy_coloring(graph)
+    end_time = time.time()
+    plot_graph(G, coloring_greedy, "./images/graph_greedy.png")
+    colors_nr = len(set(coloring_greedy)) 
+    print(f"Greedy used {colors_nr} colors.")
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time:.6f} seconds")
+    
+    start_time = time.time()
+    coloring_dsatur = dsatur.dsatur_coloring(graph)
+    end_time = time.time()
+    plot_graph(G, coloring_dsatur, "./images/graph_dsatur.png")
+    colors_nr = len(set(coloring_dsatur)) 
+    print(f"Dsatur used {colors_nr} colors.")
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time:.6f} seconds")
+        
+    print()
+    
 def main():
     good_order_crown_graph()
     bad_order_crown_graph()
     good_order_chordal_graph()
     bad_order_chordal_graph()
+    backtracking_graph()
 
 if __name__ == "__main__":
     main()
